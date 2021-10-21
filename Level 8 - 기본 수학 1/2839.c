42295d73d1aa32ee24327ee7b3b0fef482de2619
Alice <54830428+Alice-1012@users.noreply.github.com>
2021-10-22 00:52:57 +0900

    설탕 배달

--- Level 8 - 기본 수학 1/2839.c
+++ Level 8 - 기본 수학 1/2839.c
@@ -6,14 +6,15 @@ int main()
     scanf("%d",&n);
     
     while(1){
-    	if(n%5==0)
+    	if(n%5==0)//n이 5의 배수일 경우 cnt에 몫을 더하고 break
     	{
     		cnt+=n/5;
     		break;
     	}
-    	n-=3;
-    	cnt++;
-    	if(n<=0) break;
+    	n-=3;//n이 5의 배수가 아닐 경우 n-3
+    	cnt++;//3kg의 설탕봉지 개수가 1개 추가됨
+    	if(n<=0) break;//계속 반복하다가 n이 0이하면 break;
+        //n=0일 땐 3과 5의 조합으로 나누어 떨어짐, n<0일 땐 나누어 떨어지지 않음
     }
     if(n<0) printf("-1");
     else printf("%d",cnt);
