bc968afeaacf2fe72ca025ea061c46a767a84d0d
Alice <54830428+Alice-1012@users.noreply.github.com>
2021-12-13 00:47:17 +0900

    음계

---
+++ Class 1/2920.py
@@ -0,0 +1,12 @@
+num = input().split() #문자열 list
+#asc_list = [1,2,3,4,5,6,7,8]은 숫자 list라서 error
+
+asc_list = ['1','2','3','4','5','6','7','8'] 
+des_list = list(reversed(asc_list))#list를 역순으로 뒤집어서 list로 반환
+
+if num == asc_list:
+  print('ascending')
+elif num == des_list:
+  print('descending')
+else:
+  print('mixed')
