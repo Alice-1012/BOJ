1f9c5e62ec0981cae95674d8cc7a675b74071d77
SUMIN LEE <54830428+Alice-1012@users.noreply.github.com>
2022-01-15 23:20:05 +0900

    스택 수열

---
+++ Class 2/1874.py
@@ -0,0 +1,26 @@
+N = int(input())
+
+stack = []
+result = []
+count = 0
+X = True
+
+for _ in range(N):
+    num = int(input())
+    while count < num:
+        count += 1
+        stack.append(count)
+        result.append("+")
+    if stack[-1] == num:
+        stack.pop()
+        result.append("-")
+    else:
+        X = False 
+        break
+        
+if X == False:
+    print("NO")
+else:
+    for i in result:
+        print(i)
+
