3a6400c48040c11aa10ea890e769b6fa5052c373
SUMIN LEE <54830428+Alice-1012@users.noreply.github.com>
2022-02-04 23:26:58 +0900

    최소 힙

---
+++ Class 3/1927.py
@@ -0,0 +1,15 @@
+import heapq
+import sys
+n = int(sys.stdin.readline())
+
+heap = []
+for _ in range(n):
+    item = int(sys.stdin.readline())
+    if item == 0:
+        if heap:
+            print(heapq.heappop(heap))
+        else:
+            print(0)
+    else:
+        heapq.heappush(heap, item)
+
